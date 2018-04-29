4f76e1037ea99ee9fa025f7508ecdcf152c58861
Feras Deiratany <deiratany.1@osu.edu>
2018-04-29 11:59:00 -0400

    add style not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function ($) {
 
-	var textToType = [' birthplace.', ' legacy.', ' culture.', ' history.'];
-	var typeWriter = new Typewriter(textToType, document.getElementById('typedText'), 0);
-	typeWriter.play();
-
 	$('.header-area').css({
 		height: window.innerHeight+"px"
 	});
--- assets/js/typewriterIndex.js
+++ assets/js/typewriterIndex.js
@@ -2,4 +2,4 @@ jQuery(document).ready(function ($) {
   var textToType = [' birthplace.', ' legacy.', ' culture.', ' history.'];
 	var typeWriter = new Typewriter(textToType, document.getElementById('typedText'), 0);
 	typeWriter.play();
-}
+})
